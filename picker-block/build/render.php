ecfd48cb590923352e56fff12de2f541d205e80f
eoss-sh <sh@eoss.ch>
2024-02-07 13:44:16 +0100

    picker block

---
+++ picker-block/build/render.php
@@ -0,0 +1,3 @@
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Picker Block – hello from a dynamic block!', 'picker-block' ); ?>
+</p>
---
+++ picker-block/src/edit.js
@@ -0,0 +1,31 @@
+/**
+ * WP Dependencies
+ */
+import { __ } from '@wordpress/i18n'
+import { useBlockProps } from '@wordpress/block-editor'
+import { useEntityRecords } from '@wordpress/core-data'
+/**
+ * CSS
+ */
+import './editor.scss'
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit() {
+	const { records, isResolving } = useEntityRecords('postType', 'post')
+	return (
+		<div {...useBlockProps()}>
+			{__('Picker Block – hello from the editor!', 'picker-block')}
+			{!isResolving &&
+				records &&
+				records.map((record) => {
+					return h1(record.title.rendered)
+				})}
+		</div>
+	)
+}
